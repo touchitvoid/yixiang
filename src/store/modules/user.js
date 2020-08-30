b9f94fd834c40a6c8458b65dad0cb412fc82976c
jianong <jianonghe@gmail.com>
2020-08-30 23:29:49 +0800

    登录API接入

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -37,9 +37,9 @@ const user = {
     Login ({ commit }, userInfo) {
       return new Promise((resolve, reject) => {
         login(userInfo).then(response => {
-          const result = response.result
-          storage.set(ACCESS_TOKEN, result.token, 7 * 24 * 60 * 60 * 1000)
-          commit('SET_TOKEN', result.token)
+          const token = `${response.token_type} ${response.access_token}`
+          storage.set(token, response.token, 7 * 24 * 60 * 60 * 1000)
+          commit('SET_TOKEN', token)
           resolve()
         }).catch(error => {
           reject(error)
